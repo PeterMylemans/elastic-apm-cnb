b3de27bfb790d83edc870cd3948b603dbdf99992
Peter Mylemans <peter.mylemans@gmail.com>
2021-02-28 12:25:07 +0100

    Initial commit.

--- elasticapm/build_test.go
+++ elasticapm/build_test.go
@@ -62,41 +62,41 @@ func testBuild(t *testing.T, context spec.G, it spec.S) {
 		Expect(os.RemoveAll(cnbDir)).To(Succeed())
 		Expect(os.RemoveAll(workingDir)).To(Succeed())
 	})
-
-	it("returns a result that builds correctly", func() {
-		result, err := build(packit.BuildContext{
-			WorkingDir: workingDir,
-			CNBPath:    cnbDir,
-			Stack:      "some-stack",
-			BuildpackInfo: packit.BuildpackInfo{
-				Name:    "Some Buildpack",
-				Version: "some-version",
-			},
-			Plan: packit.BuildpackPlan{
-				Entries: []packit.BuildpackPlanEntry{
-					{
-						Name: "elastic-apm-agent-java",
+	context("when conditions for build are met", func() {
+		it("builds", func() {
+			result, err := build(packit.BuildContext{
+				WorkingDir: workingDir,
+				CNBPath:    cnbDir,
+				Stack:      "some-stack",
+				BuildpackInfo: packit.BuildpackInfo{
+					Name:    "Some Buildpack",
+					Version: "some-version",
+				},
+				Plan: packit.BuildpackPlan{
+					Entries: []packit.BuildpackPlanEntry{
+						{
+							Name: "elastic-apm-agent-java",
+						},
 					},
 				},
-			},
-			Layers: packit.Layers{Path: layersDir},
+				Layers: packit.Layers{Path: layersDir},
+			})
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(result.Layers).To(HaveLen(1))
+			Expect(result.Layers[0].Name).To(Equal("agent"))
+			Expect(result.Layers[0].LaunchEnv["JAVA_TOOL_OPTIONS.append"]).To(ContainSubstring("-javaagent:"))
+			Expect(result.Layers[0].LaunchEnv["JAVA_TOOL_OPTIONS.append"]).To(ContainSubstring("apm-java-agent.jar"))
+			Expect(result.Layers[0].LaunchEnv["JAVA_TOOL_OPTIONS.delim"]).To(Equal(" "))
+
+			Expect(dependencyManager.ResolveCall.Receives.Path).To(Equal(filepath.Join(cnbDir, "buildpack.toml")))
+			Expect(dependencyManager.ResolveCall.Receives.Id).To(Equal("elastic-apm-agent-java"))
+			Expect(dependencyManager.ResolveCall.Receives.Version).To(Equal("*"))
+			Expect(dependencyManager.ResolveCall.Receives.Stack).To(Equal("some-stack"))
+
+			Expect(dependencyManager.CopyCall.Receives.Dependency).To(Equal(javaAgentDependency))
+			Expect(dependencyManager.CopyCall.Receives.CnbPath).To(Equal(cnbDir))
+			Expect(dependencyManager.CopyCall.Receives.LayerPath).To(Equal(filepath.Join(layersDir, "agent/apm-java-agent.jar")))
 		})
-		Expect(err).NotTo(HaveOccurred())
-
-		Expect(result.Layers).To(HaveLen(1))
-		Expect(result.Layers[0].Name).To(Equal("agent"))
-		Expect(result.Layers[0].LaunchEnv["JAVA_TOOL_OPTIONS.append"]).To(ContainSubstring("-javaagent:"))
-		Expect(result.Layers[0].LaunchEnv["JAVA_TOOL_OPTIONS.append"]).To(ContainSubstring("apm-java-agent.jar"))
-		Expect(result.Layers[0].LaunchEnv["JAVA_TOOL_OPTIONS.delim"]).To(Equal(" "))
-
-		Expect(dependencyManager.ResolveCall.Receives.Path).To(Equal(filepath.Join(cnbDir, "buildpack.toml")))
-		Expect(dependencyManager.ResolveCall.Receives.Id).To(Equal("elastic-apm-agent-java"))
-		Expect(dependencyManager.ResolveCall.Receives.Version).To(Equal("*"))
-		Expect(dependencyManager.ResolveCall.Receives.Stack).To(Equal("some-stack"))
-
-		Expect(dependencyManager.CopyCall.Receives.Dependency).To(Equal(javaAgentDependency))
-		Expect(dependencyManager.CopyCall.Receives.CnbPath).To(Equal(cnbDir))
-		Expect(dependencyManager.CopyCall.Receives.LayerPath).To(Equal(filepath.Join(layersDir, "agent/apm-java-agent.jar")))
-
 	})
 }
